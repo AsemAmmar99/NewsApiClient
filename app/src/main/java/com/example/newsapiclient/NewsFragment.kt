c16fc8d4b8b0df9e033e0ecd6be792c900b7e439
AsemAmmar99 <asemammar99@gmail.com>
2021-10-18 02:08:36 +0200

    Info Fragment Done.

--- app/src/main/java/com/example/newsapiclient/NewsFragment.kt
+++ app/src/main/java/com/example/newsapiclient/NewsFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapiclient.data.util.Resource
 import com.example.newsapiclient.databinding.FragmentNewsBinding
 import com.example.newsapiclient.presentation.adapter.NewsAdapter
@@ -20,6 +23,10 @@ class NewsFragment : Fragment() {
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "us"
     private var page = 1
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +40,15 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding = FragmentNewsBinding.bind(view)
         viewModel= (activity as MainActivity).viewModel
         newsAdapter= (activity as MainActivity).newsAdapter
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("selected_article", it)
+            }
+            findNavController().navigate(
+                R.id.action_newsFragment_to_infoFragment,
+                bundle
+            )
+        }
         initRecyclerView()
         viewNewsList()
     }
@@ -48,6 +64,12 @@ class NewsFragment : Fragment() {
                     response.data?.let {
                         Log.i("MYTAG","came here ${it.articles.toList().size}")
                         newsAdapter.differ.submitList(it.articles.toList())
+                        if (it.totalResults%20 == 0) {
+                            pages = it.totalResults / 20
+                        }else{
+                            pages = it.totalResults / 20+1
+                        }
+                        isLastPage = page == pages
                     }
                 }
                 is Resource.Error->{
@@ -70,15 +92,44 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
     }
 
     private fun showProgressBar(){
+        isLoading = true
         fragmentNewsBinding.progressBar.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar(){
+        isLoading = false
         fragmentNewsBinding.progressBar.visibility = View.GONE
+    }
+
+    private val onScrollListener = object: RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition + visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if (shouldPaginate){
+                page++
+                viewModel.getNewsHeadlines(country, page)
+                isScrolling = false
+            }
+
+        }
     }
 
 }
